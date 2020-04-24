10353938e009eddb0d583933758ff5d9bea0e5d6
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-24 23:47:10 +0200

    Updated navbar menu

--- app/Views/inc/nav.php
+++ app/Views/inc/nav.php
@@ -10,10 +10,10 @@
 				<a class="navbar-brand" href="<?= base_url('/')?>"><span>CODEIGNITER4 TUTORIAL |</span> Simple Store Management</a>
 				<ul class="user-menu">
 					<li class="dropdown pull-right">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> <?php //echo $_SESSION['username']; ?> <span class="caret"></span></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg><span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
 							<li><a href="<?= base_url('users/profile');?>"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
-							<?= $_SESSION['username']; ?></a></li>
+							</a></li>
 							<li><a href="<?= base_url('users/login');?>"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
 						</ul>
 					</li>
