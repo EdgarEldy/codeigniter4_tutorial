ff7bb3fad3d1286c8aef5d83a877493aad1cfc4e
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-24 23:43:36 +0200

    Registered categories routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,7 +16,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('HomeController');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -30,7 +30,10 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'HomeController::index');
+$routes->get('categories', 'CategoriesController::index');
+$routes->get('categories/add','CategoriesController::add');
+$routes->post('categories/save','CategoriesController::save');
 
 /**
  * --------------------------------------------------------------------
