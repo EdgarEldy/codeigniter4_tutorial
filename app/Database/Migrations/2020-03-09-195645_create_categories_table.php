94d32bb4906c8f7baf28b0e0aaea9815d84273ac
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-24 23:46:26 +0200

    Added a migration for categories table

---
+++ app/Database/Migrations/2020-03-09-195645_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+	public function up()
+	{
+		//
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'cat_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '45'
+            ]
+        ]);
+        $this->forge->addKey('id',TRUE);
+        $this->forge->createTable('categories');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+        $this->forge->dropTable('categories');
+	}
+}
