8037ee2ccc9f51d6f62b371e3f6e3eddeaf93902
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-25 09:51:38 +0200

    Added a view for update a category

--- app/Views/categories/edit.php
+++ app/Views/categories/edit.php
@@ -1,15 +1,16 @@
+<?= $this->extend('templates/default') ?>
+<?= $this->section('content') ?>
 <div class="col-lg-12">
     <div class="panel panel-default">
-        <div class="panel-heading">Modifier</div>
+        <div class="panel-heading">Update a category</div>
         <div class="panel-body">
             <div class="col-md-6">
-                <form role="form" action="<?= URL_ROOT; ?>/categories/edit/<?= $data['id']; ?>" method="post" enctype="multipart/form-data">
-
+                <form role="form" action="<?= base_url('categories/update') ?>" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="id" value="<?= $category['id'] ?>">
                     <div class="form-group">
                         <label>Product category</label>
-                        <input type="text" name="product_name" class="form-control <?= (!empty($data['product_name_err'])) ? 'is_invalid' : '';  ?>"
-                               placeholder="" value="<?= $data['product_name']; ?>">
-                        <span class="invalid-feedback"><?= $data['product_name_err'] ?></span>
+                        <input type="text" name="cat_name" class="form-control"
+                               placeholder="" value="<?= $category['cat_name']; ?>">
                     </div>
                     <button type="submit" id="submit" class="btn btn-primary">Save</button>
                     <button type="reset" class="btn btn-default">Reset</button>
@@ -18,3 +19,4 @@
         </div>
     </div>
 </div><!-- /.col-->
+<?= $this->endSection() ?>
