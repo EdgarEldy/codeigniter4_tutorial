1830bbe55c3d9bc8c29989ba9f08b5a959a9f9b9
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-27 19:47:20 +0200

    Added a view to storing a new customer

---
+++ app/Views/customers/add.php
@@ -0,0 +1,47 @@
+<?= $this->extend('templates/default') ?>
+<?= $this->section('content') ?>
+<div class="col-lg-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">Add a customer</div>
+        <div class="panel-body">
+            <div class="col-md-6">
+                <form role="form" action="<?= base_url('customers') ?>" method="post">
+                    <div class="form-group">
+                        <label>First name :</label>
+                        <input type="text" name="first_name" class="form-control"
+                               placeholder="" value="">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <label>Last name :</label>
+                        <input type="text" name="last_name" class="form-control"
+                               placeholder="" value="">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <label>Email :</label>
+                        <input type="text" name="email" class="form-control"
+                               placeholder="" value="">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <label>Tel :</label>
+                        <input type="text" name="tel" class="form-control"
+                               placeholder="" value="">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <label>Address :</label>
+                        <input type="text" name="address" class="form-control"
+                               placeholder="" value="">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <button type="submit" id="submit" class="btn btn-primary">Save</button>
+                    <button type="reset" class="btn btn-default">Reset</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div><!-- /.col-->
+<?= $this->endSection() ?>
+								
