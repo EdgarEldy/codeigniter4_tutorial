b87dcd49c0617dbb17eda68ee819f07fdbf898f3
Edgar Eldy <ed.eldy21@live.com>
2020-10-25 18:47:19 +0200

    Errors verification while saving a category

--- app/Controllers/CategoriesController.php
+++ app/Controllers/CategoriesController.php
@@ -16,15 +16,15 @@ class CategoriesController extends Controller
      */
     public function __construct()
     {
-    $this->category = new Category();
+        $this->category = new Category();
     }
 
     public function index()
     {
-    $categories = $this->category->findAll();
-    return view('categories/index', [
-      'categories' => $categories
-    ]);
+        $categories = $this->category->findAll();
+        return view('categories/index', [
+            'categories' => $categories
+        ]);
     }
 
     public function add()
@@ -35,38 +35,45 @@ class CategoriesController extends Controller
 
     public function save()
     {
-    $cat_name = $this->request->getPost('cat_name');
-    $data = [
-      'cat_name' => $cat_name
-    ];
-    $this->category->save($data);
-    return redirect('categories');
+        $cat_name = $this->request->getPost('cat_name');
+        $data = [
+            'cat_name' => $cat_name
+        ];
+
+        if ($this->category->save($data) == false) {
+            return view('categories/add', [
+                'errors' => $this->category->errors()
+            ]);
+        } else {
+            return redirect('categories');
+        }
+
     }
 
     public function edit($id)
     {
-      $category = $this->category->find($id);
-      return view('categories/edit',[
-        'category' => $category
-      ]);
+        $category = $this->category->find($id);
+        return view('categories/edit', [
+            'category' => $category
+        ]);
     }
 
     public function update()
     {
-      // code...
-      $id = $this->request->getPost('id');
-      $cat_name = $this->request->getPost('cat_name');
-      $data = [
-        'cat_name' => $cat_name
-      ];
-      $this->category->update($id, $data);
-      return redirect('categories');
+        // code...
+        $id = $this->request->getPost('id');
+        $cat_name = $this->request->getPost('cat_name');
+        $data = [
+            'cat_name' => $cat_name
+        ];
+        $this->category->update($id, $data);
+        return redirect('categories');
     }
 
     public function delete($id)
     {
-      $this->category->delete($id);
-      return redirect('categories');
+        $this->category->delete($id);
+        return redirect('categories');
     }
 
 }
