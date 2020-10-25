73be964470905fa2fcc1653617d39e3d77837600
Edgar Eldy <ed.eldy21@live.com>
2020-10-25 19:20:14 +0200

    Update orders migration : Add columns

--- app/Database/Migrations/2020-10-25-170620_create_orders_table.php
+++ app/Database/Migrations/2020-10-25-170620_create_orders_table.php
@@ -4,15 +4,47 @@ use CodeIgniter\Database\Migration;
 
 class CreateOrdersTable extends Migration
 {
-	public function up()
-	{
-		//
-	}
+    public function up()
+    {
+        //
+        $this->db->disableForeignKeyChecks();
 
-	//--------------------------------------------------------------------
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'customer_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ],
+            'product_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ],
+            'qty' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ],
+            'total' => [
+                'type' => 'FLOAT'
+            ]
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('orders');
 
-	public function down()
-	{
-		//
-	}
+        $this->db->enableForeignKeyChecks();
+    }
+
+    //--------------------------------------------------------------------
+
+    public function down()
+    {
+        //
+        $this->forge->dropTable('orders');
+    }
 }
