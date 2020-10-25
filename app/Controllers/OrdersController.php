43e7386e9f964ca8090de323e9d083c63f528ad0
Edgar Eldy <ed.eldy21@live.com>
2020-10-25 20:57:07 +0200

    Setting customer, product and order models

--- app/Controllers/OrdersController.php
+++ app/Controllers/OrdersController.php
@@ -4,9 +4,22 @@
 namespace App\Controllers;
 
 
+use App\Models\Customer;
+use app\Models\Order;
+use App\Models\Product;
 use CodeIgniter\Controller;
 
 class OrdersController extends Controller
 {
+    protected $customer;
+    protected $product;
+    protected $order;
+    protected $db;
 
+    public function __construct()
+    {
+    	$this->customer = new Customer();
+    	$this->product = new Product();
+    	$this->order =  new Order();
+    }
 }
