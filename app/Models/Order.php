0afec273b296296c5f807dc01df6a815469cd70c
Edgar Eldy <ed.eldy21@live.com>
2020-11-07 19:56:44 +0200

    Update Order model : Add fields

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,12 +1,24 @@
 <?php
 
 
-namespace app\Models;
+namespace App\Models;
 
 
 use CodeIgniter\Model;
 
 class Order extends Model
 {
+    protected $DBGroup = 'default';
+    protected $table = 'orders';
+    protected $primaryKey = 'id';
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
 
+    protected $allowedFields = ['customer_id', 'product_id', 'qty', 'total'];
+
+    protected $useTimestamps = true;
+
+    protected $createdField = '';
+    protected $updatedField = '';
+    protected $deletedField = '';
 }
