4fb6d6744f0da9f1fe21db771a3249a193cce573
Edgar Eldy <ed.eldy21@live.com>
2020-11-20 22:19:38 +0200

    Add a function for getting orders

--- app/Controllers/OrdersController.php
+++ app/Controllers/OrdersController.php
@@ -4,6 +4,7 @@
 namespace App\Controllers;
 
 
+use App\Models\Category;
 use App\Models\Customer;
 use App\Models\Order;
 use App\Models\Product;
@@ -13,27 +14,29 @@ use Config\Database;
 class OrdersController extends Controller
 {
     protected $customer;
+    protected $category;
     protected $product;
     protected $order;
     protected $db;
 
     public function __construct()
     {
-    	$this->customer = new Customer();
-    	$this->product = new Product();
-    	$this->order =  new Order();
-    	$this->db = Database::connect();
+        $this->customer = new Customer();
+        $this->category = new Category();
+        $this->product = new Product();
+        $this->order = new Order();
+        $this->db = Database::connect();
     }
 
     public function index()
     {
         $builder = $this->db->table('customers');
-        $builder->join('orders','orders.customer_id = customers.id');
-        $builder->join('products','orders.product_id = products.id');
+        $builder->join('orders', 'orders.customer_id = customers.id');
+        $builder->join('products', 'orders.product_id = products.id');
         $orders = $builder->get();
 
-        return view('orders/index',[
-           'orders' => $orders
+        return view('orders/index', [
+            'orders' => $orders
         ]);
     }
 }
