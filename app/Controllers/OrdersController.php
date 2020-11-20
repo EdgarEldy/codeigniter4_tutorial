bed777059ec4302c3470888bbf7aea430f3e7da7
Edgar Eldy <ed.eldy21@live.com>
2020-11-20 22:22:07 +0200

    Add a function for getting orders/add view with customers and categories data

--- app/Controllers/OrdersController.php
+++ app/Controllers/OrdersController.php
@@ -21,22 +21,32 @@ class OrdersController extends Controller
 
     public function __construct()
     {
-        $this->customer = new Customer();
-        $this->category = new Category();
-        $this->product = new Product();
-        $this->order = new Order();
-        $this->db = Database::connect();
+    	$this->customer = new Customer();
+    	$this->category = new Category();
+    	$this->product = new Product();
+    	$this->order =  new Order();
+    	$this->db = Database::connect();
     }
 
     public function index()
     {
         $builder = $this->db->table('customers');
-        $builder->join('orders', 'orders.customer_id = customers.id');
-        $builder->join('products', 'orders.product_id = products.id');
+        $builder->join('orders','orders.customer_id = customers.id');
+        $builder->join('products','orders.product_id = products.id');
         $orders = $builder->get();
 
-        return view('orders/index', [
-            'orders' => $orders
+        return view('orders/index',[
+           'orders' => $orders
+        ]);
+    }
+
+    public function add()
+    {
+        $customers = $this->customer->findAll();
+        $categories = $this->category->findAll();
+        return view('orders/add',[
+            'customers' => $customers,
+            'categories' => $categories,
         ]);
     }
 }
