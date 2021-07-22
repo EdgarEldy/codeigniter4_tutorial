d3a2b573b7552c00bdbc5bd9d925e69b451dbf83
Edgar Eldy <ed.eldy21@live.com>
2021-07-22 21:16:10 +0200

    Add database password

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends \CodeIgniter\Database\Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => '',
+		'password' => '123456',
 		'database' => 'codeigniter4_db',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
