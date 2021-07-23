2a6e9c4ff88a457c0a82521f0ad4f42687d48c77
Edgar Eldy <ed.eldy21@live.com>
2021-07-23 13:41:00 +0200

    Get unit price by product id ajax

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,14 @@ $(function () {
     });
   });
 });
+
+//Get unit price by product id ajax
+$(function () {
+    $("#product_id").on("change", function () {
+        var product_id = $(this).val();
+        // alert(product_id);
+        $.get("/orders/getUnitPrice", { product_id: product_id }, function (data) {
+                $("#unit_price").val(data);
+            });
+    });
+});
