b91b8147fbfa6d3b3820d9d6a58e4c28418fb243
Edgar Eldy <ed.eldy21@live.com>
2021-07-23 13:41:34 +0200

    Add orders/getUnitPrice route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -62,6 +62,7 @@ $routes->get('orders/edit/(:any)','OrdersController::edit/$1');
 $routes->post('orders/update','OrdersController::update');
 $routes->post('orders/delete/(:any)','OrdersController::delete/$1');
 $routes->get('orders/getProducts', 'OrdersController::getProducts');
+$routes->get('orders/getUnitPrice', 'OrdersController::getUnitPrice');
 
 /**
  * --------------------------------------------------------------------
