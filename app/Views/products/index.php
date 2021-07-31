34295c07ffa816cf793f124e726858a56a41bd2d
Edgar Eldy <ed.eldy21@live.com>
2021-07-31 19:57:58 +0200

    Show flash message after saving product

--- app/Views/products/index.php
+++ app/Views/products/index.php
@@ -4,39 +4,38 @@
     <div class="panel panel-default">
         <div class="panel-heading">Products</div>
         <div class="panel-body">
-            <a href="<?= base_url('products/add') ?>"
-               class="btn btn-primary">New</a>
-            <table data-toggle="table" data-url=""  data-show-refresh="true" data-show-toggle="true"
-                   data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true"
-                   data-sort-name="name" data-sort-order="desc">
+            <?php if (session()->getFlashdata('product_saved')) : ?>
+                <?php echo '<p class="alert alert-success w-100 h-100">' . session()->getFlashdata('product_saved') . '</p>'; ?>
+            <?php endif; ?>
+            <a href="<?= base_url('products/add') ?>" class="btn btn-primary">New</a>
+            <table data-toggle="table" data-url="" data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
                 <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Category</th>
-                    <th>Product name</th>
-                    <th>Unit price</th>
-                    <th>Options</th>
-                </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Category</th>
+                        <th>Product name</th>
+                        <th>Unit price</th>
+                        <th>Options</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <?php foreach($products->getResult('array') as $product): ?>
-                <tr>
-                    <td><?= $product['id']; ?></td>
-                    <td><?= $product['category_name']; ?></td>
-                    <td><?= $product['product_name']; ?></td>
-                    <td><?= $product['unit_price']; ?></td>
-                    <td>
-                        <div class="card-footer">
-                            <a href="products/edit/<?= $product['id']; ?>"
-                               class="btn btn-primary">Edit</a>
-                            <form action="products/delete/<?= $product['id']; ?>" method="post">
-                                <button type="submit" onclick="return confirm('Are you sure you want to delete this product ?')" class="btn btn-danger btn-sm">Delete</button>
-                            </form>
+                    <?php foreach ($products->getResult('array') as $product) : ?>
+                        <tr>
+                            <td><?= $product['id']; ?></td>
+                            <td><?= $product['category_name']; ?></td>
+                            <td><?= $product['product_name']; ?></td>
+                            <td><?= $product['unit_price']; ?></td>
+                            <td>
+                                <div class="card-footer">
+                                    <a href="products/edit/<?= $product['id']; ?>" class="btn btn-primary">Edit</a>
+                                    <form action="products/delete/<?= $product['id']; ?>" method="post">
+                                        <button type="submit" onclick="return confirm('Are you sure you want to delete this product ?')" class="btn btn-danger btn-sm">Delete</button>
+                                    </form>
 
-                        </div>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
