985a4fa44b40eea7aa723c919fae8bba5a9cc7d2
Edgar Eldy <ed.eldy21@live.com>
2021-07-31 20:20:06 +0200

    Show flash message after saving order

--- app/Views/orders/index.php
+++ app/Views/orders/index.php
@@ -4,45 +4,44 @@
     <div class="panel panel-default">
         <div class="panel-heading">Orders</div>
         <div class="panel-body">
-            <a href="<?= base_url('orders/add') ?>"
-               class="btn btn-primary">New</a>
-            <table data-toggle="table" data-url=""  data-show-refresh="true" data-show-toggle="true"
-                   data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true"
-                   data-sort-name="name" data-sort-order="desc">
+            <?php if (session()->getFlashdata('order_saved')) : ?>
+                <?php echo '<p class="alert alert-success w-100 h-100">' . session()->getFlashdata('order_saved') . '</p>'; ?>
+            <?php endif; ?>
+            <a href="<?= base_url('orders/add') ?>" class="btn btn-primary">New</a>
+            <table data-toggle="table" data-url="" data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
                 <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Fist name</th>
-                    <th>Last name</th>
-                    <th>Product name</th>
-                    <th>Unit price</th>
-                    <th>Qty</th>
-                    <th>Total</th>
-                    <th>Options</th>
-                </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Fist name</th>
+                        <th>Last name</th>
+                        <th>Product name</th>
+                        <th>Unit price</th>
+                        <th>Qty</th>
+                        <th>Total</th>
+                        <th>Options</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <?php foreach($orders->getResult('array') as $order): ?>
-                <tr>
-                    <td><?= $order['order_id']; ?></td>
-                    <td><?= $order['first_name']; ?></td>
-                    <td><?= $order['last_name']; ?></td>
-                    <td><?= $order['product_name']; ?></td>
-                    <td><?= $order['unit_price']; ?></td>
-                    <td><?= $order['qty']; ?></td>
-                    <td><?= $order['total']; ?></td>
-                    <td>
-                        <div class="card-footer">
-                            <a href="orders/edit/<?= $order['order_id']; ?>"
-                               class="btn btn-primary">Edit</a>
-                            <form action="orders/delete/<?= $order['order_id']; ?>" method="post">
-                                <button type="submit" onclick="return confirm('Are you sure you want to delete this order ?')" class="btn btn-danger btn-sm">Delete</button>
-                            </form>
+                    <?php foreach ($orders->getResult('array') as $order) : ?>
+                        <tr>
+                            <td><?= $order['order_id']; ?></td>
+                            <td><?= $order['first_name']; ?></td>
+                            <td><?= $order['last_name']; ?></td>
+                            <td><?= $order['product_name']; ?></td>
+                            <td><?= $order['unit_price']; ?></td>
+                            <td><?= $order['qty']; ?></td>
+                            <td><?= $order['total']; ?></td>
+                            <td>
+                                <div class="card-footer">
+                                    <a href="orders/edit/<?= $order['order_id']; ?>" class="btn btn-primary">Edit</a>
+                                    <form action="orders/delete/<?= $order['order_id']; ?>" method="post">
+                                        <button type="submit" onclick="return confirm('Are you sure you want to delete this order ?')" class="btn btn-danger btn-sm">Delete</button>
+                                    </form>
 
-                        </div>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
