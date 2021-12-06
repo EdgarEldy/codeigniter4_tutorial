0a99c226f6056e3e620de91145c1f0b545b25f00
Edgar Eldy <ed.eldy21@live.com>
2021-12-06 21:49:23 +0200

    Update User model : Add validations

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,4 +11,32 @@ class User extends Model
     protected $primaryKey = 'id';
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
+
+    // Add validations
+    protected $validationRules = [
+        'profile_id' => 'required',
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'email' => 'required',
+        'password' => 'required'
+    ];
+    protected $validationMessages = [
+        'profile_id' => [
+            'required' => 'The profile name is required'
+        ],
+        'first_name' => [
+            'required' => 'The first name is required'
+        ],
+        'last_name' => [
+            'required' => 'The last name is required'
+        ],
+        'email' => [
+            'required' => 'The email is required',
+            'is_unique' => 'This email already exists. Please add another'
+        ],
+        'password' => [
+            'required' => 'The password is required'
+        ]
+    ];
+    protected $skipValidation = false;
 }
