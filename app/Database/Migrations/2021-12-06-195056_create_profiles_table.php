7fc6561c94ec4b0dc0e94a4cde4e0cb9833d4207
Edgar Eldy <ed.eldy21@live.com>
2021-12-06 21:53:48 +0200

    Update profiles migration : Add columns

--- app/Database/Migrations/2021-12-06-195056_create_profiles_table.php
+++ app/Database/Migrations/2021-12-06-195056_create_profiles_table.php
@@ -6,13 +6,26 @@ class CreateProfilesTable extends Migration
 {
 	public function up()
 	{
-		//
+		$this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'profile_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '45'
+            ]
+        ]);
+        $this->forge->addKey('id',TRUE);
+        $this->forge->createTable('profiles');
 	}
 
 	//--------------------------------------------------------------------
 
 	public function down()
 	{
-		//
+        $this->forge->dropTable('profiles');
 	}
 }
