64e9d6451124bb78a3eabd72c3589ba87c4b1892
Edgar Eldy <ed.eldy21@live.com>
2021-12-06 21:58:10 +0200

    Update users migration : Add columns

--- app/Database/Migrations/2021-12-06-195407_create_users_table.php
+++ app/Database/Migrations/2021-12-06-195407_create_users_table.php
@@ -6,13 +6,48 @@ class CreateUsersTable extends Migration
 {
 	public function up()
 	{
-		//
+		$this->db->disableForeignKeyChecks();
+
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'profile_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ],
+            'first_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ],
+			'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+			],
+			'password' => [
+				'type' => 'VARCHAR',
+				'constraint' => '255'
+			]
+        ]);
+        $this->forge->addKey('id',TRUE);
+        $this->forge->addForeignKey('profile_id','profiles','id','CASCADE','CASCADE');
+        $this->forge->createTable('users');
+
+        $this->db->enableForeignKeyChecks();
 	}
 
 	//--------------------------------------------------------------------
 
 	public function down()
 	{
-		//
+		$this->forge->dropTable('users');
 	}
 }
